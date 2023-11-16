fbf72d23d939093a17afe6497c574a9869bf7780
Parvesh Kumar <parveshkumarbaloda@gmail.com>
2023-11-16 17:55:43 +0530

    Background changes
    Animated the background components
    Made background blurry

--- src/threejs.tsx
+++ src/threejs.tsx
@@ -1,96 +1,103 @@
-import { createRoot } from 'react-dom/client';
-import React, { useRef, useState } from 'react';
-import { ConeGeometry, Mesh } from 'three';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { createRoot } from "react-dom/client";
+import React, { useRef, useState } from "react";
+import { ConeGeometry, Mesh } from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { ListFormat } from "typescript";
 
-
-export default function BG(props:any) {
-    return(
-        <div style={{height: 0}}>
-            <Canvas style={{position: 'absolute'}}>
-              <ambientLight />
-              <pointLight position={[10, 10, 10]} />
-              <Box position={[-3, 0, 0]} />
-              <Cone position={[0, 0, 0]} />
-              <Sphere position={[3, 0, 0]} />
-            </Canvas>,
-        </div>
-    )
+export default function BG(props: any) {
+  return (
+    <div style={{ height: 0 }}>
+      <Canvas style={{ position: "absolute" }}>
+        <ambientLight intensity={0.7}/>
+        <pointLight position={[10, 10, 10]} intensity={100}/>
+        <Box y_min={-1} y_max={1.3} position={[-4, 0, 0]} />
+        <Cone y_min={-1} y_max={2} position={[0, 1.5, 0]} />
+        <Sphere y_min={-1} y_max={1.6} position={[4, 1.6, 0]} />
+        {/* <Cone position={[0, 0, 0]} />
+        <Sphere position={[3, 0, 0]} /> */}
+      </Canvas>
+      ,
+    </div>
+  );
 }
 
 function Box(props:any) {
-  // This reference will give us direct access to the mesh
-  const meshRef = useRef<Mesh>(null!)
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-  // Return view, these are regular three.js elements expressed in JSX
+  var y_direction: number = 1;
+  const meshRef = useRef<Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta;
+    if (meshRef.current.position.y <= props.y_min) y_direction = 1;
+    if (meshRef.current.position.y >= props.y_max) y_direction = -1;
+    meshRef.current.position.y += delta * y_direction;
+  });
   return (
     <mesh
-      {...props}
+    //   {...props}
+      position={props.position}
       ref={meshRef}
       scale={active ? 1.5 : 1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
+      onPointerOut={(event) => setHover(false)}
+    >
       <boxGeometry args={[1.5, 1.5, 1.5]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <meshStandardMaterial color={"dee2ff"} />
     </mesh>
-  )
+  );
 }
 
 function Cone(props:any) {
-    var y_max:number = 2;
-    var y_min:number = -2;
-    var y_direction:number = 1;
-    // This reference will give us direct access to the mesh
-    const meshRef = useRef<Mesh>(null!)
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((state, delta) => {
-      meshRef.current.rotation.x += delta;
-      if( meshRef.current.position.y <= y_min ) y_direction = 1;
-      if( meshRef.current.position.y >= y_max ) y_direction = -1;
-      meshRef.current.position.y += delta*y_direction;
-    })
-    // Return view, these are regular three.js elements expressed in JSX
-    return (
-      <mesh
-        {...props}
-        ref={meshRef}
-        scale={active ? 1.5 : 1}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}>
-        <coneGeometry args={[1, 2, 16]}/>
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    )
-}
-
-function Sphere(props:any) {
-  // This reference will give us direct access to the mesh
-  const meshRef = useRef<Mesh>(null!)
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-  // Return view, these are regular three.js elements expressed in JSX
+  var y_direction: number = 1;
+  const meshRef = useRef<Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta;
+    if (meshRef.current.position.y <= props.y_min) y_direction = 1;
+    if (meshRef.current.position.y >= props.y_max) y_direction = -1;
+    meshRef.current.position.y += delta * y_direction;
+  });
   return (
     <mesh
-      {...props}
+    //   {...props}
+      position={props.position}
       ref={meshRef}
       scale={active ? 1.5 : 1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <sphereGeometry args={[1, 12, 8]}/>
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      onPointerOut={(event) => setHover(false)}
+    >
+      <coneGeometry args={[1, 2, 12]} />
+      <meshStandardMaterial color={"dee2ff"} />
     </mesh>
-  )
+  );
+}
+
+function Sphere(props:any) {
+  var y_direction: number = 1;
+  const meshRef = useRef<Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta;
+    if (meshRef.current.position.y <= props.y_min) y_direction = 1;
+    if (meshRef.current.position.y >= props.y_max) y_direction = -1;
+    meshRef.current.position.y += delta * y_direction;
+  });
+  return (
+    <mesh
+    //   {...props}
+      position={props.position}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <sphereGeometry args={[1, 16, 16]} />
+      <meshStandardMaterial color={"dee2ff"} />
+    </mesh>
+  );
 }
